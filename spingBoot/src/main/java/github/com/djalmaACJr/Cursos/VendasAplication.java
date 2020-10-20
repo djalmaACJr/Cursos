3b0471ae9a8204254c6bdb41568e43e9ad133f72
djalma <djalma@tacom.com.br>
2020-10-20 19:23:43 -0300

    curso spring

---
+++ spingBoot/src/main/java/github/com/djalmaACJr/Cursos/VendasAplication.java
@@ -0,0 +1,11 @@
+package github.com.djalmaACJr.Cursos;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class VendasAplication {
+    public static void main(String[] args) {
+
+    }
+
+}
